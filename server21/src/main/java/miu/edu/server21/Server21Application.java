f8f75aa57c213c04a718c4dd80f44f42cd3f8a6c
umur inan <umurinan@gmail.com>
2020-08-03 15:20:48 -0500

    server21

---
+++ server21/src/main/java/miu/edu/server21/Server21Application.java
@@ -0,0 +1,13 @@
+package miu.edu.server21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server21Application.class, args);
+    }
+
+}
