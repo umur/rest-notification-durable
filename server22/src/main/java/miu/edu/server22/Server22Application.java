d78bacfafefb7ad276af25eb9010c5db4ce03b73
umur inan <umurinan@gmail.com>
2020-08-03 15:23:35 -0500

    server22

---
+++ server22/src/main/java/miu/edu/server22/Server22Application.java
@@ -0,0 +1,13 @@
+package miu.edu.server22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server22Application.class, args);
+    }
+
+}
---
+++ server22/src/test/java/miu/edu/server22/Server22ApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.server22;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Server22ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
