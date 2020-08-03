2bda5a6200483593e820d1dfabb2e771e6c75df8
umur inan <umurinan@gmail.com>
2020-08-03 15:26:18 -0500

    server23

---
+++ server23/src/main/java/miu/edu/server23/Server23Application.java
@@ -0,0 +1,13 @@
+package miu.edu.server23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server23Application.class, args);
+    }
+
+}
---
+++ server23/src/test/java/miu/edu/server23/Server23ApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.server23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Server23ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
