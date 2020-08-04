32d47bf6fb886b6b4a25c86b7a623075a33c7801
umur inan <umurinan@gmail.com>
2020-08-04 05:52:43 -0500

    server25

---
+++ server25/src/main/java/miu/edu/server25/Server25Application.java
@@ -0,0 +1,13 @@
+package miu.edu.server25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Server25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Server25Application.class, args);
+    }
+
+}
---
+++ server25/src/test/java/miu/edu/server25/Server25ApplicationTests.java
@@ -0,0 +1,13 @@
+package miu.edu.server25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Server25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
